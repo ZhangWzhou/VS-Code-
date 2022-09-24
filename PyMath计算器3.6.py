6c242b0c289cf92312cab8fe1dd564be8d67c2a8
张文舟 <109419405+ZhangWzhou@users.noreply.github.com>
2022-09-24 14:24:10 +0800

    Update and rename PyMath计算器3.4.py to PyMath计算器3.6.py

--- PyMath计算器3.6.py
+++ PyMath计算器3.6.py
@@ -4,18 +4,18 @@ Created on Thur Aug 25 11:41 2022
 @author: zhang
 """
 
-# PyMath计算器3.4.1
+# PyMath计算器3.6.1
 # 使用须知：
 # 1.本产品由Zhang Wenzhou于2022年8月研发
 # 2.开放源代码 https://github.com/ZhangWzhou/ZhangWenzhou/blob/main/PyMath%E8%AE%A1%E7%AE%97%E5%99%A83.4.py
 # 3.免费
 
-print("欢迎使用PyMath计算器 3.4.1!")
+print("欢迎使用PyMath计算器 3.6.1!")
 print("本计算器提供算术运算：加法（+）、减法（-）、乘法（*）、除法（/）、取余除（%）、取整除（//）、乘方（**）、开方（sqrt()）八种运算；逻辑运算："
       "and，or，not三种运算；比较运算：>=，==，<=,>，<五种运算；转换类型：int()整数类型、float()浮点类型、str()字符串类型。")
 print("可以调用NumPy（模块名np）、Pandas（模块名pd）、random（模块名rd）函数。")
 print("提供科学绘图(matplotlib)，使用时模块名使用plt。注意：一个提示符后只能写一行代码！不可对变量进行赋值！")
-print("可以调用π(pi)。")
+print("可以调用π(pi)以及所有math模块的函数。")
 print("如果看到>>>提示符即可输入运算表达式，按回车运算。")
 print("如需终止运算请输入exit()。")
 print("---------------------------------------------------------------------------------------------------------------")
@@ -23,7 +23,7 @@ print("-------------------------------------------------------------------------
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from math import sqrt, pi
+import math
 import random as rd
 
 while True:
